84cd0f21d7945a526df42040a7e0bd6d9fffba88
Zack Shorts <shorts23@gmail.com>
2019-03-28 18:22:25 -0600

    testing with retweet

--- bot.js
+++ bot.js
@@ -1,40 +1,9 @@
 // Our Twitter library
-var Twit = require('twit');
-var TwitterBot = require('node-twitterbot').TwitterBot;
-var config = require('./config.js');
-var Bot = new TwitterBot(config);
-// We need to include our configuration file
-var T = new Twit(require('./config.js'));
-
-// This is the URL of a search for the latest tweets on the '#MeetMaye' hashtag.
-var mediaArtsSearch = {q: "#MeetMaye", count: 100, result_type: "recent"};
-
-// This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
-function retweetLatest() {
-    T.get('search/tweets', mediaArtsSearch, function (error, data) {
-        // log out any errors and responses
-        console.log(error, data);
-        // If our search request to the server had no errors...
-        if (!error) {
-            // ...then we grab the ID of the tweet we want to retweet...
-            var retweetId = data.statuses[0].id_str;
-            // ...and then we tell Twitter we want to retweet it!
-            T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
-                if (response) {
-                    console.log('Success! Check your bot, it should have retweeted something.')
-                }
-                // If there was an error with our Twitter call, we print it out here.
-                if (error) {
-                    console.log('There was an error with Twitter:', error);
-                }
-            })
-        }
-        // However, if our original search request had an error, we want to print it out here.
-        else {
-            console.log('There was an error with your hashtag search:', error);
-        }
-    });
-}
+const Twit = require('twit');
+const TwitterBot = require('node-twitterbot').TwitterBot;
+const config = require('./config.js');
+const Bot = new TwitterBot(config);
+const T = new Twit(require(config));
 
 function jumbleText(tweet) {
     let text = tweet.split('');
@@ -47,9 +16,9 @@ function jumbleText(tweet) {
 }
 
 function listen() {
-    var userID = '138203134';
+    let userID = '138203134';
 // open a stream following events from that user ID
-    var stream = T.stream('statuses/filter', {follow: (userID)});
+    let stream = T.stream('statuses/filter', {follow: (userID)});
 
     stream.on('tweet', function (tweet) {
         if (tweet.user.id_str === userID) {
@@ -58,14 +27,11 @@ function listen() {
             console.log('Found this lil guy: ' + phrase);
             Bot.tweet(phrase)
         } else {
-            //This is a retweet :)
+            let phrase = jumbleText(tweet.text);
+            console.log("This is a retweet :) \n" + phrase);
+            Bot.tweet(phrase)
         }
     });
 }
 
 listen();
-// Try to retweet something as soon as we run the program...
-// retweetLatest();
-// ...and then every hour/half after that. Time here is in milliseconds, so
-// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-// setInterval(retweetLatest, 1000 * 60 * 30);
